486ccdc7ffc4e93ce9883d0559bcd31f70df1ea8
nikkimeratnia <niki.meratnia@gmail.com>
2026-09-13 09:31:55 +0330

    Add BugService and fix BugRepository package

--- src/main/java/com/nikki/thebugtracker/BugRepository.java
+++ src/main/java/com/nikki/thebugtracker/BugRepository.java
@@ -1,3 +1,5 @@
+package com.nikki.thebugtracker;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface BugRepository extends JpaRepository<Bug,Long> {
--- src/main/java/com/nikki/thebugtracker/BugService.java
+++ src/main/java/com/nikki/thebugtracker/BugService.java
@@ -1,4 +1,17 @@
 package com.nikki.thebugtracker;
 
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
 public class BugService {
+    private final BugRepository bugRepository;
+    public BugService(BugRepository bugRepository){
+        this.bugRepository=bugRepository;
+    }
+    public void save(){
+        List<String>bugs = new ArrayList<>();
+    }
 }
